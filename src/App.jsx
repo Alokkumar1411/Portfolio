de2266099c876799a6f6ad363b3d008870edf357
Alokkumar1411 <2k24.cs1c.2414120@gmail.com>
2026-04-06 20:21:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero from './components/Hero'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <div className='h-screen w-screen bg-black'>
+
+      </div>
+      <Hero/>
+      <div className='h-screen w-screen bg-black'>
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
